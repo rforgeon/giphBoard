d4d3e5474b23dbbc4d3141fa8fabf7f3a21af634
rforgeon <rforgeon@gmail.com>
2016-08-16 11:11:24 -0700

    index

---
+++ giphBoard/engine.js
@@ -0,0 +1,73 @@
+/**
+Giphbord allows you to create a story board of multiple giphs.
+
+Information we want to store:
+>Slide Object
+  -giph embed url
+  -text option
+>GiphBoard Object
+  -Author
+  -Date/time created (auto generate)
+  -Array of Slide Objects
+*/
+
+var slideID = 0;
+
+//constructor function
+function GiphBoard(author){
+  this.author = author;
+  this.slides = [];
+}
+
+GiphBoard.prototype.addSlide = function(slide){
+  this.slides.push(slide);
+}
+
+GiphBoard.prototype.removeSlide = function(slideID){
+  for (var i=0; i< this.slides.length; i++){
+    if (slideID === this.slides[i].slideID){
+      this.slides.splice(i,1);
+      return {success: true, message: "Your slide was deleted."}
+    }
+  }
+  return {success: false, message: "Your slide has not been deleted."}
+}
+
+GiphBoard.prototype.moveSlide = function(fromIndex,toIndex){
+  var slideIndex = this.slides.splice(fromIndex,1);
+  this.slides.splice(toIndex,0,slideIndex[0]);
+}
+
+
+function Slide(url, slideText){
+  this.url = url;
+  this.slideText = slideText;
+  this.slideID = slideID++;
+}
+
+//Main///////////
+
+// var firstGiph = new GiphBoard("ralph");
+//
+// console.log(firstGiph);
+//
+// firstGiph.addSlide(new Slide("url","#1"));
+// firstGiph.addSlide(new Slide("url","#2"));
+// firstGiph.addSlide(new Slide("url","#3"));
+//
+// console.log(firstGiph.slides);
+//
+// console.log(firstGiph.removeSlide(2));
+//
+// console.log(firstGiph.slides);
+//
+// firstGiph.moveSlide(1,0);
+//
+// console.log(firstGiph.slides);
+//
+// firstGiph.addSlide(new Slide("url","#3"));
+// firstGiph.addSlide(new Slide("url","#4"));
+//
+// firstGiph.moveSlide(3,1);
+//
+// console.log(firstGiph.slides);
---
+++ giphBoard/view.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+  $("#giphContainer").css("background","#F00").css("width","100%").css("height","400px").text("Hello World!");
+});
